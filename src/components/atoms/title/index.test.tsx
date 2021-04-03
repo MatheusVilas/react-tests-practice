aabfac8b346add6d23327162fbf8b9676385b7ac
MatheusVilas <matheus@vilasboas.dev>
2021-04-03 12:10:01 -0300

    refactor: removing atomic folder

--- src/components/atoms/title/index.test.tsx
+++ src/components/atoms/title/index.test.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { getQueriesForElement } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Title from "./index";
 import faker from "faker";
 
-import * as ReactDOM from "react-dom";
-
 test("tilte must have children printed as text", () => {
-  const root = document.createElement("div");
   const randomText = faker.lorem.words(30);
 
-  ReactDOM.render(<Title>{randomText}</Title>, root);
-
-  const { getByText } = getQueriesForElement(root);
+  const { getByText } = render(<Title>{randomText}</Title>);
 
   expect(getByText(randomText)).not.toBe(null);
 
